925206c6692e7a8d8e65a22cf16accf5dca94ffb
João Pedro G. C. <115590969+jpgercc@users.noreply.github.com>
2025-05-17 13:30:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#o código está um ****, não faz muito sentido, mas, espero que seja util, foi o GPT que fez então culpe ele qualquer coisa, minha unica culpa é ser vagabundo.
 
 # -*- coding: utf-8 -*-
 """Calcula a Gematria de um número usando métodos guloso e tradicional."""
